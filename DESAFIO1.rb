d20e7c37f893e6b3d521d19476dc544d029f95c1
RUY SOUZA NETO <ruyneto1@gmail.com>
2023-09-01 18:52:32 -0300

    Add files via upload

---
+++ DESAFIO1.rb
@@ -0,0 +1,13 @@
+print "Digite seu nome pfv: "
+
+nome = gets.chomp
+
+print "Agora digite seu sobrenome pfvr: "
+
+sobrenome = gets.chomp
+
+print "Informe sua idade pfvr: "
+
+idade = gets.chomp
+
+puts "Seu nome completo é #{nome} #{sobrenome} e sua idade é #{idade}"
